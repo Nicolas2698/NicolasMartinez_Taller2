42cad207e37fd2daba093061a3e1b838bb922309
Nicolas Martinez <nicolas2698@outlook.com>
2018-11-01 12:14:28 -0500

    se crea la pagina de albums

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 /*app.get('/agregarDocumento', function(req, res){
     const collection = db.collection('albums');
 
@@ -18,17 +16,18 @@
 
 
 
-const express = require('express'), 
+const express = require('express'),
     engines = require('consolidate'),
     MongoClient = require('mongodb').MongoClient;
-    //ObjectID = require('mongodb').ObjectID;
-    
-var app = express(),db;
+//ObjectID = require('mongodb').ObjectID;
 
-    const dbName = 'tienda';
+var app = express(),
+    db;
+
+const dbName = 'tienda';
 
 app.engine('hbs', engines.handlebars);
-app.set ('views', './views');
+app.set('views', './views');
 app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
@@ -41,39 +40,66 @@ MongoClient.connect('mongodb://localhost:27017', function (err, client) {
     app.listen(3000);
 });
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.get('/tienda', function(req, res){
+app.get('/tienda', function (req, res) {
     const collection = db.collection('productos');
     const collectionTwo = db.collection('albums');
     let dokss;
-    collectionTwo.find().toArray(function(err, doks){
-        if(err){
-          console.error(err);
-          res.send(err);
-          return;
+
+    collectionTwo.find().toArray(function (err, doks) {
+        if (err) {
+            console.error(err);
+            res.send(err);
+            return;
         }
         dokss = doks;
-      });
-
-    collection.find().toArray(function(err, docs){
-      if(err){
-        console.error(err);
-        res.send(err);
-        return;
-      }
-  
-
-      var contexto = {
-        products: docs,
-        albums: dokss
-      };
-
-      console.log(docs)
-      res.render('tienda', contexto);
     });
 
-    
-  });
+    collection.find().toArray(function (err, docs) {
+        if (err) {
+            console.error(err);
+            res.send(err);
+            return;
+        }
+
+
+        const contexto = {
+            products: docs,
+            albums: dokss
+        };
+
+        console.log(docs)
+        res.render('tienda', contexto);
+    });
+
+
+});
+
+app.get("/tienda/producto", function (req, res) {
+    var nombreProducto = req.query.title;
+    console.log(nombreProducto);
+    const collectionTwo = db.collection('albums');
+    collectionTwo.find({}).toArray(function(err, docs){
+        var cualquiera = docs.find(function(obj){
+           return obj.title == nombreProducto; 
+        });
+        res.render('albums', cualquiera);
+    });
+
+    //console.log(req.query.title);
+
+    /* collectionTwo.filter({
+         title: req.query.title
+     });
+
+     collectionTwo.toArray((err,result) => {
+         //console.log('hi server')
+         res.render('albums', {
+             producto: result[0],
+         });
+     });
+     */
+});
