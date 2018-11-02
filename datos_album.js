27293144fc7dc711cb893b370179a74cf8d9ff88
Nicolas Martinez <nicolas2698@outlook.com>
2018-11-01 20:07:33 -0500

    Se quitan unos archivos del gitignore

---
+++ datos_album.js
@@ -0,0 +1,62 @@
+[{
+        title: "Pray for the wicked",
+        songs: 11,
+        year: 2018,
+        duration: "34:11",
+        genre: ["pop rock", "barroque rock"],
+        image: "./imgs/album.png",
+        price: "$19,99",
+        song: ["1. (Fuck A) Silver Lining", "2. Say Amen (Saturday Night)", "3. Hey Look Ma, I Made It", "4. High Hopes" ,"5. Roaring 20s", "6. Dancing's Not a Crime", "7. One of the Drunks", "8. The Overpass", "9. King of the Clouds", "10. Old Fashioned", "11. Dying in LA"]
+    },
+    {
+        title: "Death of a Bachelor",
+        songs: 11,
+        year: 2016,
+        duration: "36:00",
+        genre: ["rock", "pop"],
+        image: "./imgs/album3.png",
+        price: "$19,99",
+        song: ["Victorious", "Don't Threaten Me With a Good Time", "Hallelujah", "Emperor's New Clothes", "Death of a Bachelor", "Crazy=Genius", "LA Devotee", "Golden Days", "The Good, The Bad and the Dirty", "House of Memories", "Impossible Year"
+            ]
+    },
+    {
+        title: "Too Weird to Live, Too Rare to Die!",
+        songs: 10,
+        year: 2013,
+        duration: "32:32",
+        genre: ["alternative", "independent"],
+        image: "./imgs/album2.png",
+        price: "$12,99",
+        song: ["This Is Gospel", "Miss Jackson (ft. Lolo)", "Vegas Lights", "Girl That You Love", "Nicotine", "Girls/Girls/Boys", "Casual Affair", "Far Too Young to Die", "Collar Full", "The End of All Things"]
+    },
+    {
+        title: "Vices and Virtues",
+        songs: 10,
+        year: 2011,
+        duration: "37:27",
+        genre: ["pop rock", "barroque rock"],
+        image: "./imgs/album4.jpg",
+        price: "$7,99",
+        song:["The Ballad of Mona Lisa", "Let's Kill Tonight", "Hurricane", "Memories", "Trade Mistakes", "Ready to Go (Get Me Out of My Mind)", "Always", "The Calendar", "Ryan Smiles", "Nearly Witches (Ever Since We Met...)"]
+    },
+    {
+        title: "A Fever You Can't Sweat Out",
+        songs: 13,
+        year: 2005,
+        duration: "40:15",
+        genre: ["rock", "alternative"],
+        image: "./imgs/album5.jpg",
+        price: "$7,99",
+        song:["Introduction»	0:37", "The Only Difference Between Martyrdom and Suicide Is Press Coverage", "London Beckoned Songs About Money Written by Machines", "Nails for Breakfast, Tacks for Snacks", "Camisado", "Time to Dance", "Lying Is the Most Fun a Girl Can Have Without Taking Her Clothes Off", "Intermission", "But It's Better If You Do", "I Write Sins Not Tragedies", "I Constantly Thank God for Esteban", "There's a Good Reason These Tables Are Numbered Honey, You Just Haven't Thought of It Yet", "Build God, Then We'll Talk"]
+    },
+    {
+        title: "Pretty Odd",
+        songs: 15,
+        year: 2008,
+        duration: "48:46",
+        genre: ["independent", "pop"],
+        image: "./imgs/album6.jpg",
+        price: "$7,99",
+        song:["We're So Starving", "Nine In the Afternoon", "She's a Handsome Woman", "Do You Know What I'm Seeing?", "That Green Gentleman (Things Have Changed)", "I Have Friends In Holy Spaces", "Northern Downpour", "When the Day Met the Night", "Pas de Cheval", "The Piano Knows Something I Don't Know", "Behind the Sea", "Folkin' Around", "She Had the World", "From a Mountain In the Middle of the Cabins", "Mad as Rabbits"]
+    }
+]
